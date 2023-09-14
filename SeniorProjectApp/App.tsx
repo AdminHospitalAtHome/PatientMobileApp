10ccb12c47c792b7aaa45a268ea8c2c8e1e995c0
JiangFanGang <jiangy10@rose-hulman.edu>
2023-09-13 21:26:55 -0400

    Login init

--- SeniorProjectApp/App.tsx
+++ SeniorProjectApp/App.tsx
@@ -4,7 +4,7 @@
  *
  * @format
  */
-
+<script src="http://localhost:8097" />;
 import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
@@ -15,103 +15,58 @@ import {
   Text,
   useColorScheme,
   View,
+  Image,
+  TextInput,
+  Pressable,
+  Button,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+function App(): JSX.Element {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <Image
+        source={require('./Images/unionHealthLogo.png')}
+        style={styles.logo}
+        resizeMethod={'auto'}
+      />
+      <View>
+        <Text style={styles.inputLabel}>Username:</Text>
+        <TextInput style={styles.inputBox} />
+      </View>
+      <View>
+        <Text style={styles.inputLabel}>Password: </Text>
+        <TextInput secureTextEntry style={styles.inputBox} />
+      </View>
+
+      <View style={{flexDirection: 'row'}}>
+        <Button title={'Signup'} />
+        <Button title={'Login'} />
+      </View>
     </View>
   );
 }
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
+  inputLabel: {
     fontSize: 18,
-    fontWeight: '400',
   },
-  highlight: {
-    fontWeight: '700',
+  inputBox: {
+    borderRadius: 5,
+    width: 230,
+    height: 40,
+    borderColor: 'grey',
+    borderWidth: 1,
+    marginBottom: 10,
+  },
+  button: {},
+  logo: {
+    height: 110,
   },
 });
 
