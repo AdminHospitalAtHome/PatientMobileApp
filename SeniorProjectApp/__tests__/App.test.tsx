364bdbee29d4f0d2d4c94e2fb8afde4c8bb8f322
rhit-wallisjm <wallisjm@rose-hulman.edu>
2023-09-20 14:37:44 -0400

    Updated Tests
    Co-Authored-By: rhit-weiy5 <78141717+rhit-weiy5@users.noreply.github.com>

--- SeniorProjectApp/__tests__/App.test.tsx
+++ SeniorProjectApp/__tests__/App.test.tsx
@@ -7,11 +7,17 @@ import React from 'react';
 import App from '../App';
 
 // Note: import explicitly to use the types shiped with jest.
-import {it} from '@jest/globals';
+import {it, expect} from '@jest/globals';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   renderer.create(<App />);
+});
+
+// Test UploadWeight API
+it('Upload Data to UploadWeight correctly', () => {
+  let s = 200;
+  expect(s).toBe(200);
 });
