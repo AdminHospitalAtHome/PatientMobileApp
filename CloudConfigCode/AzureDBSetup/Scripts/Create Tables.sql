39654fb7ca663c339b7e7ae56faec2dab4f82b33
rhit-wallisjm <wallisjm@rose-hulman.edu>
2023-09-24 20:04:25 -0400

    Create new SQL Tables
    We all sat down together and created the new tables
    
    Co-Authored-By: rhit-jiangy10 <90019374+jiangy10@users.noreply.github.com>
    Co-Authored-By: rhit-ken <11023512+llkenll@users.noreply.github.com>
    Co-Authored-By: rhit-weiy5 <78141717+rhit-weiy5@users.noreply.github.com>

--- CloudConfigCode/AzureDBSetup/Scripts/Create Tables.sql
+++ CloudConfigCode/AzureDBSetup/Scripts/Create Tables.sql
@@ -14,3 +14,31 @@ CREATE TABLE Patient_Weight (
 	WeightInPounds DECIMAL(4,1) NOT NULL, -- allows one decimal point and 3 numbers in fornt (e.g. 123.1)
 	IfManualInput BIT NOT NULL -- 1 is yes, 0 if no
 );
+
+-- Create Blood Pressure Table
+CREATE TABLE Patient_Blood_Pressure (
+	UniqueID INT IDENTITY(1,1) NOT NULL PRIMARY KEY, -- Unique ID for entry
+	PatientID VARCHAR(20) FOREIGN KEY REFERENCES Patient_Info(PatientID), -- May change based on client Needs
+	DateTimeTaken DateTime NOT NULL,
+	SystolicBloodPressureInMmHg Int NOT NULL,
+	DiastolicBloodPressureInMmHg Int NOT NULL,
+	IfManualInput BIT NOT NULL -- 1 is yes, 0 if no
+)
+
+-- Create Blood Oxygen Level Table
+CREATE TABLE Patinet_Blood_Oxygen (
+	UniqueID INT IDENTITY(1,1) NOT NULL PRIMARY KEY, -- Unique ID for entry
+	PatientID VARCHAR(20) FOREIGN KEY REFERENCES Patient_Info(PatientID), -- May change based on client Needs
+	DateTimeTaken DateTime NOT NULL,
+	BloodOxygenLevelInPercentage Int NOT NULL,
+	IfManualInput BIT NOT NULL -- 1 is yes, 0 if no
+)
+
+-- Create Heart Rate Table
+CREATE TABLE Patinet_Heart_Rate (
+	UniqueID INT IDENTITY(1,1) NOT NULL PRIMARY KEY, -- Unique ID for entry
+	PatientID VARCHAR(20) FOREIGN KEY REFERENCES Patient_Info(PatientID), -- May change based on client Needs
+	DateTimeTaken DateTime NOT NULL,
+	HeartRateInBPM Int NOT NULL,
+	IfManualInput BIT NOT NULL -- 1 is yes, 0 if no
+)
