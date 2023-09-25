b1059dd4bd77608fe847b44fb2d4e66a0895677c
rhit-wallisjm <wallisjm@rose-hulman.edu>
2023-09-25 13:57:11 -0400

    Azure Code For getWeight
    Co-Authored-By: rhit-weiy5 <78141717+rhit-weiy5@users.noreply.github.com>

--- CloudConfigCode/AzureFuctionAppSetup/getWeight/index.js
+++ CloudConfigCode/AzureFuctionAppSetup/getWeight/index.js
@@ -1,7 +1,7 @@
 module.exports = async function (context, req, weight) {
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
+        status: 200, /* Defaults to 200 */
         body: weight
     };
 }
