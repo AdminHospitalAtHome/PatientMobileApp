6d901748bc6f8e9ade2a5cecf7296c7fc3805ac1
rhit-wallisjm <wallisjm@rose-hulman.edu>
2023-10-09 15:57:55 -0400

    Updated SQL to meet client requests
    Co-Authored-By: rhit-ken <11023512+llkenll@users.noreply.github.com>

--- CloudConfigCode/AzureDBSetup/Scripts/Create Tables.sql
+++ CloudConfigCode/AzureDBSetup/Scripts/Create Tables.sql
@@ -1,24 +1,25 @@
 CREATE TABLE Patient_Info (
-	PatientID VARCHAR(20) NOT NULL PRIMARY KEY,
+	PatientID INT NOT NULL PRIMARY KEY,
 	FirstName VARCHAR(20) NOT NULL,
 	LastName VARCHAR(20) NOT NULL,
-	DateOfBirth DATE NOT NULL
+	DateOfBirth DATE NOT NULL,
+	IfAccessibilityMode BIT
 );
 
 
 -- Create Patient Weight Table
 CREATE TABLE Patient_Weight (
 	UniqueID INT IDENTITY(1,1) NOT NULL PRIMARY KEY, -- Unique ID for entry
-	PatientID VARCHAR(20) FOREIGN KEY REFERENCES Patient_Info(PatientID), -- May change based on client Needs
+	PatientID INT FOREIGN KEY REFERENCES Patient_Info(PatientID), -- May change based on client Needs
 	DateTimeTaken DateTime NOT NULL,
-	WeightInPounds DECIMAL(4,1) NOT NULL, -- allows one decimal point and 3 numbers in fornt (e.g. 123.1)
+	WeightInPounds INT NOT NULL,
 	IfManualInput BIT NOT NULL -- 1 is yes, 0 if no
 );
 
 -- Create Blood Pressure Table
 CREATE TABLE Patient_Blood_Pressure (
 	UniqueID INT IDENTITY(1,1) NOT NULL PRIMARY KEY, -- Unique ID for entry
-	PatientID VARCHAR(20) FOREIGN KEY REFERENCES Patient_Info(PatientID), -- May change based on client Needs
+	PatientID INT FOREIGN KEY REFERENCES Patient_Info(PatientID), -- May change based on client Needs
 	DateTimeTaken DateTime NOT NULL,
 	SystolicBloodPressureInMmHg Int NOT NULL,
 	DiastolicBloodPressureInMmHg Int NOT NULL,
@@ -28,7 +29,7 @@ CREATE TABLE Patient_Blood_Pressure (
 -- Create Blood Oxygen Level Table
 CREATE TABLE Patinet_Blood_Oxygen (
 	UniqueID INT IDENTITY(1,1) NOT NULL PRIMARY KEY, -- Unique ID for entry
-	PatientID VARCHAR(20) FOREIGN KEY REFERENCES Patient_Info(PatientID), -- May change based on client Needs
+	PatientID INT FOREIGN KEY REFERENCES Patient_Info(PatientID), -- May change based on client Needs
 	DateTimeTaken DateTime NOT NULL,
 	BloodOxygenLevelInPercentage Int NOT NULL,
 	IfManualInput BIT NOT NULL -- 1 is yes, 0 if no
@@ -37,7 +38,7 @@ CREATE TABLE Patinet_Blood_Oxygen (
 -- Create Heart Rate Table
 CREATE TABLE Patinet_Heart_Rate (
 	UniqueID INT IDENTITY(1,1) NOT NULL PRIMARY KEY, -- Unique ID for entry
-	PatientID VARCHAR(20) FOREIGN KEY REFERENCES Patient_Info(PatientID), -- May change based on client Needs
+	PatientID INT FOREIGN KEY REFERENCES Patient_Info(PatientID), -- May change based on client Needs
 	DateTimeTaken DateTime NOT NULL,
 	HeartRateInBPM Int NOT NULL,
 	IfManualInput BIT NOT NULL -- 1 is yes, 0 if no
