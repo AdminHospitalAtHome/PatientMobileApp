865f31cc7fc8d7e8887089e9cf8e6d4cc5dc2618
rhit-wallisjm <wallisjm@rose-hulman.edu>
2023-12-12 14:59:24 -0500

    Working on Testing issues

--- SeniorProjectApp/__tests__/App.test.tsx
+++ SeniorProjectApp/__tests__/App.test.tsx
@@ -5,9 +5,15 @@
 import 'react-native';
 import React from 'react';
 import App from '../App';
-import {it, expect} from '@jest/globals';
+import {it, expect, jest} from '@jest/globals';
 import renderer from 'react-test-renderer';
-
+import window from '../__mocks__/window';
+// import window from
+// jest.mock('@azure/communication-common');
+// jest.mock('@azure/communication-chat');
+// jest.spyOn(window.Navigator, 'userAgent');
+// jest.mock('node-libs-react-native/mock/net');
+// jest.mock('node-libs-react-native/mock/tls');
 it('renders correctly', () => {
   renderer.create(<App />);
 });
--- SeniorProjectApp/jest.config.js
+++ SeniorProjectApp/jest.config.js
@@ -1,3 +1,13 @@
+/** @type {import('jest').Config} */
+
+const config = {
+  transformIgnorePatterns: ["node_modules/(?!(react-native|my-project|react-native-button)/)"],
+}
+
+
 module.exports = {
   preset: 'react-native',
+  transformIgnorePatterns: ["/node_modules/(?!(@react-native|react-native|react-native-button|react-native-gifted-chat|react-native-parsed-text|react-native-lightbox-v2|react-native-typing-animation|uuid)/)","/node_modules/node-libs-react-native"],
 };
+
+
